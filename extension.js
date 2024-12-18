53ecf80b9b582865492d4b43e968512ede7050d4
Jack Pts <jackpts1981@gmail.com>
2024-12-18 23:41:43 +0300

    [gnome.ext.song.title] handling bash script run

--- extension.js
+++ extension.js
@@ -3,12 +3,13 @@ import * as PanelMenu from "resource:///org/gnome/shell/ui/panelMenu.js";
 import St from "gi://St";
 import Clutter from "gi://Clutter";
 import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
+import GLib from "gi://GLib";
 
 export default class SongTitleExtension extends Extension {
   enable() {
     this.button = new PanelMenu.Button(0.0, this.metadata.name, false);
     const text = new St.button({
-      text: "Song name test!",
+      text: this.runBashScript(),
       x_align: Clutter.ActorAlign.CENTER,
       y_align: Clutter.ActorAlign.CENTER,
       style_class: "my-custom-button",
@@ -21,5 +22,21 @@ export default class SongTitleExtension extends Extension {
   disable() {
     this.button?.destroy();
     this.button = null;
+  }
+
+  runBashScript() {
+    let [success, stdout, stderr] = GLib.spawn_command_line_sync(
+      "bash ./song_name.sh",
+    );
+
+    if (success) {
+      // Output from the script
+      let output = GLib.file_get_contents(stdout);
+      log("Script Output: " + output);
+      return output;
+    } else {
+      log("Error: " + stderr);
+      return "Error in bash exec!";
+    }
   }
 }
--- song_name.sh
+++ song_name.sh
@@ -2,6 +2,7 @@
 
 # Fetch the current song title from ncmpcpp
 line_count=$(mpc | grep "" -c)
+# TODO: handle the case when `mpc` isn't installed
 
 if [ $line_count -gt 1 ]; then
   echo "$(mpc | head -n1)"
