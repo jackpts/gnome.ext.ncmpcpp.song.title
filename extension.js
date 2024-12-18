1e4d09c2f60bf780fd27b6f4e37e4e21715d2b45
Jack Pts <jackpts1981@gmail.com>
2024-12-18 20:00:45 +0300

    [gnome.ext.song.title] imports fix

--- extension.js
+++ extension.js
@@ -1,17 +1,34 @@
-const St = imports.gi.St;
-const Main = imports.ui.main;
+import * as Main from "resource:///org/gnome/shell/ui/main.js";
+// import * as St from "resource:///org/gnome/gjs/modules/st.js";
+import St from "gi://St";
+// import Clutter from "resource:///org/gnome/gjs/modules/clutter.js";
+import Clutter from "gi://Clutter";
+import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
 
-let indicator;
+export default class SongTitleExtension extends Extension {
+  // class SongTitleExtension {
+  // constructor() {
+  //   super();
+  //   this.label = null;
+  // }
 
-function init() {
-  //init
+  enable() {
+    this.label = new St.Label({
+      text: "Song name test!",
+      x_align: Clutter.ActorAlign.CENTER,
+      y_align: Clutter.ActorAlign.CENTER,
+      style_class: "my-custom-label",
+    });
+
+    Main.panel.addToStatusArea("my-label", this.label);
+  }
+
+  disable() {
+    this.label?.destroy();
+    this.label = null;
+  }
 }
 
-function enable() {
-  indicator = new St.Label({ text: "Song name test!" });
-  Main.panel.addToStatusArea("my-indicator", indicator);
-}
-
-function disable() {
-  indicator.destroy();
-}
+// function init() {
+//   return new SongTitleExtension();
+// }
