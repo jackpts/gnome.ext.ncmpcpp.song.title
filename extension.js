8bf5b80ff916f416a49f5764eb48978b1ea77c43
Jack Pts <jackpts1981@gmail.com>
2024-12-18 23:05:53 +0300

    [gnome.ext.song.title] errors fix, test text output

--- extension.js
+++ extension.js
@@ -1,34 +1,25 @@
 import * as Main from "resource:///org/gnome/shell/ui/main.js";
-// import * as St from "resource:///org/gnome/gjs/modules/st.js";
+import * as PanelMenu from "resource:///org/gnome/shell/ui/panelMenu.js";
 import St from "gi://St";
-// import Clutter from "resource:///org/gnome/gjs/modules/clutter.js";
 import Clutter from "gi://Clutter";
 import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
 
 export default class SongTitleExtension extends Extension {
-  // class SongTitleExtension {
-  // constructor() {
-  //   super();
-  //   this.label = null;
-  // }
-
   enable() {
-    this.label = new St.Label({
+    this.button = new PanelMenu.Button(0.0, this.metadata.name, false);
+    const text = new St.button({
       text: "Song name test!",
       x_align: Clutter.ActorAlign.CENTER,
       y_align: Clutter.ActorAlign.CENTER,
-      style_class: "my-custom-label",
+      style_class: "my-custom-button",
     });
 
-    Main.panel.addToStatusArea("my-label", this.label);
+    this.button.add_child(text);
+    Main.panel.addToStatusArea("my-button", this.button);
   }
 
   disable() {
-    this.label?.destroy();
-    this.label = null;
+    this.button?.destroy();
+    this.button = null;
   }
 }
-
-// function init() {
-//   return new SongTitleExtension();
-// }
