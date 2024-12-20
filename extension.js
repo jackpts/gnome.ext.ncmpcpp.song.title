acbd2e8fc18aa4e6e3ef54668bfce36f3be5fb7e
Jack Pts <jackpts1981@gmail.com>
2024-12-20 20:50:00 +0300

    [gnome.ext.song.title] script up

--- extension.js
+++ extension.js
@@ -8,8 +8,11 @@ import GLib from "gi://GLib";
 export default class SongTitleExtension extends Extension {
   enable() {
     this.button = new PanelMenu.Button(0.0, this.metadata.name, false);
+    // const decoder = new TextDecoder('utf-8');
+    // const fileName = decoder.decode(this.runBashScript());
+    const songTitle = this.getSongTitle("bash ./song_title.sh");
     const text = new St.button({
-      text: this.runBashScript(),
+      text: songTitle,
       x_align: Clutter.ActorAlign.CENTER,
       y_align: Clutter.ActorAlign.CENTER,
       style_class: "my-custom-button",
@@ -17,6 +20,7 @@ export default class SongTitleExtension extends Extension {
 
     this.button.add_child(text);
     Main.panel.addToStatusArea("my-button", this.button);
+    // Main.panel.statusArea['my-extension'].set_label(output);
   }
 
   disable() {
@@ -24,19 +28,26 @@ export default class SongTitleExtension extends Extension {
     this.button = null;
   }
 
-  runBashScript() {
-    let [success, stdout, stderr] = GLib.spawn_command_line_sync(
-      "bash ./song_name.sh",
-    );
+  getSongTitle(command) {
+    try {
+      let [success, stdout, stderr, exit_status] = GLib.spawn_command_line_sync(
+        command,
+      );
+      log('sss [success, stdout, stderr, exit_status]=' + [success, stdout, stderr, exit_status]);
+      log("sss typeof stdout: " + typeof stdout);
 
-    if (success) {
-      // Output from the script
-      let output = GLib.file_get_contents(stdout);
-      log("Script Output: " + output);
-      return output;
-    } else {
-      log("Error: " + stderr);
-      return "Error in bash exec!";
+      if (success) {
+        // Output from the script
+        let output = GLib.file_get_contents(stdout);
+        log("Script Output: " + output);
+        return output.toString().trim();
+      } else {
+        log("Error: " + stderr);
+        return "Error in bash exec!";
+      }
+    } catch (e) {
+        log("Error: " + e);
+        return null;
     }
   }
 }
